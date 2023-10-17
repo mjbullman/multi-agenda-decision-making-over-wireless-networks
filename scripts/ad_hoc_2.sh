05ca05d13101c91d5d36de553ecfb77fcac3d76f
Martin Bullman <admin@quickminutes.com>
2023-10-17 10:36:59 +0300

    Intel Galileo wireless networking configuration scripts for each device

---
+++ scripts/ad_hoc_2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#				 Insight Center for Data Analytics
+#					University College Cork
+#					
+#                  Ad-hoc networking setup code 2
+#     
+#   @author - Martin Bullman    <martinbullman@live.ie>        
+#   @since - 03/07/2014
+
+ip link set wlan0 down
+iwconfig wlan0 mode ad-hoc
+iwconfig wlan0 channel 4
+iwconfig wlan0 essid 'GalileoAD-HOC'
+iwconfig wlan0 ap AA:BB:CC:DD:EE:FF
+iwconfig wlan0 key 1234567890
+ip link set wlan0 up
+ip addr add 192.168.1.222/24 dev wlan0
